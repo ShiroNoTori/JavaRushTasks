f3f82d084263c280fd08414c97d34dbd4b66195f
ShiroNoTori <accelerator1569@gmail.com>
2018-10-07 19:20:53 +0300

    Java Syntaxis task 0109 completed.

--- 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0109/Solution.java
@@ -6,6 +6,7 @@ package com.javarush.task.task01.task0109;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int a = 3;
+        int b = 2;
     }
 }
