e48f4f2d13cb5500f87734a3c254b0bb0e345c3c
ShiroNoTori <accelerator1569@gmail.com>
2018-10-10 16:46:56 +0300

    Java Syntaxis task 0121 attempt 1.

--- 1.JavaSyntax/src/com/javarush/task/task01/task0121/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0121/Solution.java
@@ -6,6 +6,12 @@ package com.javarush.task.task01.task0121;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        String s1 = "Я согласен на зарплату ";
+        System.out.println(s1 + "$800/месяц в первый год.");
+        System.out.println(s1 + "$1500/месяц во второй год.");
+        System.out.println(s1 + "$2200/месяц в третий год.");
+        System.out.println(s1 + "$2700/месяц в четвёртый год.");
+        System.out.println(s1 + "$3200/месяц в пятый год.");
+        System.out.println("\nПоцелуй мой блестящий металлический зад!");
     }
 }
