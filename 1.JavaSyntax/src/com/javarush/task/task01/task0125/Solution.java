49158238870fbf53d56ff60a5ed45b73db810577
ShiroNoTori <accelerator1569@gmail.com>
2018-10-10 16:59:53 +0300

    Java Syntaxis task 0125 completed.

--- 1.JavaSyntax/src/com/javarush/task/task01/task0125/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0125/Solution.java
@@ -6,12 +6,12 @@ package com.javarush.task.task01.task0125;
 
 public class Solution {
     public static void main(String[] args) {
-        String s = "15";
-        a = 5;
-        int z = 18;
+        //String s = "15";
+        int a = 5;
+        //int z = 18;
         int d = 18;
-        c = a + d;
-        b = " better than ";
+        int c = a + d;
+        String b = " better than ";
 
         System.out.println(a + b + c);
     }
