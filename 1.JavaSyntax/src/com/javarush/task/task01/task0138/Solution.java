19ad104bce3f40e979fc4e866133bb3478cff066
ShiroNoTori <accelerator1569@gmail.com>
2018-10-10 17:25:50 +0300

    Java Syntaxis task 0138 completed.

--- 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -7,6 +7,11 @@ package com.javarush.task.task01.task0138;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Мое любимое стихотворение:");
-        //напишите тут ваш код
+        System.out.println("Кто нравом тяжел,\n" +
+                "тот всех осуждает,\n" +
+                "смеется над всем;\n" +
+                "ему невдомек,\n" +
+                "а должен бы знать,\n" +
+                "что сам он с изъяном.\n");
     }
 }
