65d8a24e3eaf2110cfc6effb98b62f733efee4ae
ShiroNoTori <accelerator1569@gmail.com>
2018-10-15 18:47:39 +0300

    Java Syntaxis task 0216 completed.

--- 1.JavaSyntax/src/com/javarush/task/task02/task0216/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task02/task0216/Solution.java
@@ -5,8 +5,8 @@ package com.javarush.task.task02.task0216;
 */
 public class Solution {
     public static int min(int a, int b, int c) {
-        if (a < b && a < c )return a;
-        else if (b < a && b < c) return b;
+        if (a <= b && a <= c )return a;
+        else if (b <= a && b <= c) return b;
         else return c;
     }
 
