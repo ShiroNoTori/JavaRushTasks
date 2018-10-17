bda437404428907992d4dd457ace5b4b306d29e3
ShiroNoTori <accelerator1569@gmail.com>
2018-10-18 01:56:27 +0300

    Java Syntaxis task 0303 attempt1.

--- 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0303/Solution.java
@@ -6,10 +6,11 @@ package com.javarush.task.task03.task0303;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        convertEurToUsd(10, 1.3);
+        convertEurToUsd(100, 1.4);
     }
 
     public static double convertEurToUsd(int eur, double course) {
-        //напишите тут ваш код
+        return (eur*course);
     }
 }
