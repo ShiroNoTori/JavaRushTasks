5ae7b4ac6025d69e4ae10c048bc34faed038b93c
ShiroNoTori <accelerator1569@gmail.com>
2018-10-18 02:51:48 +0300

    Java Syntaxis task 0307 completed.

--- 1.JavaSyntax/src/com/javarush/task/task03/task0307/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0307/Solution.java
@@ -6,7 +6,36 @@ package com.javarush.task.task03.task0307;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Zerg zerg1 = new Zerg();
+        Zerg zerg2 = new Zerg();
+        Zerg zerg3 = new Zerg();
+        Zerg zerg4 = new Zerg();
+        Zerg zerg5 = new Zerg();
+
+        zerg1.name = "Abathur";
+        zerg2.name = "Kerrigan";
+        zerg3.name = "Zagara";
+        zerg4.name = "Dehaka";
+        zerg5.name = "Broodmother";
+
+        Protoss protoss1 = new Protoss();
+        Protoss protoss2 = new Protoss();
+        Protoss protoss3 = new Protoss();
+
+        protoss1.name = "Alarak";
+        protoss2.name = "Arthanis";
+        protoss3.name = "Tassadar";
+
+        Terran terran1 = new Terran();
+        Terran terran2 = new Terran();
+        Terran terran3 = new Terran();
+        Terran terran4 = new Terran();
+
+        terran1.name = "Raynor";
+        terran2.name = "Tycus";
+        terran3.name = "Sara";
+        terran4.name = "Nova";
+
     }
 
     public static class Zerg {
