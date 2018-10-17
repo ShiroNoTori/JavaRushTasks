172d9a6d08575b53fdc0b4c480f8145abcd7e372
ShiroNoTori <accelerator1569@gmail.com>
2018-10-18 02:56:56 +0300

    Java Syntaxis task 0308 completed.

--- 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0308/Solution.java
@@ -1,11 +1,15 @@
 package com.javarush.task.task03.task0308;
 
-/* 
+/*
 Произведение 10 чисел
 */
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int result = 1;
+        for (int i = 1; i <= 10; i++){
+            result *= i;
+        }
+        System.out.print(result);
     }
 }
