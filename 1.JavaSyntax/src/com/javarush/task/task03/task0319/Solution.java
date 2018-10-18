18a5fe22ac219b42dd1cc29c8705700126ab757b
ShiroNoTori <accelerator1569@gmail.com>
2018-10-18 04:13:25 +0300

    Java Syntaxis task 0319 completed.

--- 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -5,9 +5,16 @@ package com.javarush.task.task03.task0319;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int summ = sc.nextInt();
+        int time = sc.nextInt();
+
+        System.out.println(name + " получает " + summ + " через " + time + " лет.");
     }
 }
