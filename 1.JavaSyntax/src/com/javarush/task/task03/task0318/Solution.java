7e3a79470352924a666bc34814568fb43b866e0d
ShiroNoTori <accelerator1569@gmail.com>
2018-10-18 04:20:31 +0300

    Java Syntaxis task 0318 Не вижу ошибок.

--- 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0318/Solution.java
@@ -5,9 +5,15 @@ package com.javarush.task.task03.task0318;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int years = sc.nextInt();
+
+        System.out.print(name + " захватит мир через " + years + " лет. Му-ха-ха!");
     }
 }
