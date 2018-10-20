36f19742b29403a749a7d84dcdb6123e52a726bb
ShiroNoTori <accelerator1569@gmail.com>
2018-10-21 00:09:31 +0300

    Java Syntaxis task 0325 completed.

--- 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0325/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task03.task0325;
 
 import java.io.*;
+import java.util.Scanner;
 
 /* 
 Финансовые ожидания
@@ -8,6 +9,9 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println("Я буду зарабатывать $" + n + " в час");
     }
 }
