e4cf4d18b6fb00c67448912f2737262c23d892af
ShiroNoTori <accelerator1569@gmail.com>
2018-10-21 23:31:46 +0300

    Java Syntaxis task 0412 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0412/Solution.java
@@ -5,11 +5,18 @@ package com.javarush.task.task04.task0412;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a < 0)
+            a++;
+        else if (a > 0)
+            a *= 2;
 
+        System.out.println(a);
     }
 
 }
