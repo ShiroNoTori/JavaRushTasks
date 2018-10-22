e3b82d819259bc9bdc99e30e1a8c267e992a0d2e
ShiroNoTori <accelerator1569@gmail.com>
2018-10-22 17:32:50 +0300

    Java Syntaxis task 0414 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0414/Solution.java
@@ -5,9 +5,17 @@ package com.javarush.task.task04.task0414;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if ((year%4 == 0 && year%100 != 0) || year%400 == 0){
+            System.out.println("количество дней в году: 366");
+        } else {
+            System.out.println("количество дней в году: 365");
+        }
     }
 }
