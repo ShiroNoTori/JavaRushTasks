10c7300fae1350d8b7fa5c9296252344fdac2aa9
ShiroNoTori <accelerator1569@gmail.com>
2018-10-22 22:20:11 +0300

    Java Syntaxis task 0415 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0415/Solution.java
@@ -5,9 +5,18 @@ package com.javarush.task.task04.task0415;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if ((a < (b + c)) && (b < (a + c)) && (c < (a + b)))
+            System.out.println("Треугольник существует.");
+        else System.out.println("Треугольник не существует.");
     }
 }
