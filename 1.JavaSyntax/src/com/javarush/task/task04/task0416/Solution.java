e685ac1c37332fcfdfae23ecc7e14099c55ed6f6
ShiroNoTori <accelerator1569@gmail.com>
2018-10-22 23:09:13 +0300

    Java Syntaxis task 0416 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0416/Solution.java
@@ -5,9 +5,19 @@ package com.javarush.task.task04.task0416;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+
+        Scanner sc = new Scanner(System.in);
+
+        float t = sc.nextFloat();
+
+        if (t%5 >= 3 && t%5 < 4)
+            System.out.println("жёлтый");
+        else if (t%5 >= 4 && t%5 < 5)
+            System.out.println("красный");
+        else System.out.println("зелёный");
     }
 }
