dc7fdbca12ca6108637ad2c8fb703b617df76d64
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 16:57:09 +0300

    Java Syntaxis task 0419 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0419/Solution.java
@@ -5,9 +5,21 @@ package com.javarush.task.task04.task0419;
 */
 
 import java.io.*;
+import java.math.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        System.out.println(Math.max(Math.max(a, b), Math.max(c, d)));
+
+
     }
 }
