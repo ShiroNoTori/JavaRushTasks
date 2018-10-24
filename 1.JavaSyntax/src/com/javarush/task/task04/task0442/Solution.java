770b79e8340d413d9494af7fd73d87b25ab15a2c
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 20:27:44 +0300

    Java Syntaxis task 0442 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0442/Solution.java
@@ -6,9 +6,21 @@ package com.javarush.task.task04.task0442;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        int summ = 0;
+        boolean stop = false;
+        int a = 0;
+
+        while(!stop){
+            a = sc.nextInt();
+            stop = (a == -1);
+            summ += a;
+        }
+        System.out.println(summ);
     }
 }
