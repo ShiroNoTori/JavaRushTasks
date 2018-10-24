a2a6a3d753862b7ed69b2dcf2460ef18370aa54d
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 17:58:24 +0300

    Java Syntaxis task 0421 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0421/Solution.java
@@ -5,9 +5,18 @@ package com.javarush.task.task04.task0421;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        String name1 = sc.nextLine();
+        String name2 = sc.nextLine();
+
+        if (name1.equals(name2))
+            System.out.println("Имена идентичны");
+        else if (name1.length() == name2.length())
+            System.out.println("Длины имен равны");
     }
 }
