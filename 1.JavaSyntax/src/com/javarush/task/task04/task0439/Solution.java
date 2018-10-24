9d7eaf676248f731aa58b72988db17465c65f465
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 19:50:47 +0300

    Java Syntaxis task 0439 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0439/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0439/Solution.java
@@ -5,10 +5,17 @@ package com.javarush.task.task04.task0439;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+
+        for (int i = 0; i < 10; i++){
+            System.out.println(name + " любит меня.");
+        }
 
     }
 }
