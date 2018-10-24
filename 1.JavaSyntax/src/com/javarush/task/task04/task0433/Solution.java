d01210fbdacc0b8a84dad5707140e49592f43ce3
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 19:16:09 +0300

    Java Syntaxis task 0433 completed.

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0433/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task04.task0433;
+
+
+/* 
+Гадание на долларовый счет
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+
+        int i = 10, j = 10;
+
+        while (i > 0){
+
+            while (j > 0){
+                System.out.print("S");
+                j--;
+            }
+            System.out.println();
+            i--;
+            j = 10;
+        }
+
+    }
+}
