a3e71491d2a15fff7190ddeff2088ff3be6341b2
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 18:11:44 +0300

    Java Syntaxis task 0424 completed. Не вижу смысла выдумывать какой-либо алгоритм поиска.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0424/Solution.java
@@ -5,9 +5,24 @@ package com.javarush.task.task04.task0424;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        int[] m = new int[3];
+
+        for (int i = 0; i < m.length; i++)
+            m[i] = sc.nextInt();
+
+        if(m[0] == m[1] || m[1] == m[2] || m[0] == m[2]){
+            if (m[0] != m[1] && m[0] != m[2])
+                System.out.println(1);
+            else if (m[1] != m[0] && m[1] != m[2])
+                System.out.println(2);
+            else if (m[2] != m[0] && m[2] != m[1])
+                System.out.println(3);
+        }
     }
 }
