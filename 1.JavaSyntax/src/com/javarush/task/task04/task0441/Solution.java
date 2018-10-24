f22ae508c53cb4332dee00661792ddafcd927d39
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 20:19:01 +0300

    Java Syntaxis task 0441 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0441/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0441/Solution.java
@@ -5,9 +5,32 @@ package com.javarush.task.task04.task0441;
 Как-то средненько
 */
 import java.io.*;
+import java.math.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        int[] m = new int[3];
+
+        m[0] = sc.nextInt();
+        m[1] = sc.nextInt();
+        m[2] = sc.nextInt();
+
+        if (m[0] == m[1] || m[1] == m[2] || m[0] == m[2]){
+            if(m[0] == m[1])
+                System.out.println(m[0]);
+            else System.out.println(m[2]);
+            return;
+        }
+
+        int max = Math.max(Math.max(m[0], m[1]), m[2]);
+        int min = Math.min(Math.min(m[0], m[1]), m[2]);
+
+        for (int i = 0; i < m.length; i++){
+            if (m[i] != max && m[i] != min)
+                System.out.println(m[i]);
+        }
     }
 }
