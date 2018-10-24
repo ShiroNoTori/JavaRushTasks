678175c6a47d38871ffcfe5d93dd4e88b7a461ec
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 19:24:58 +0300

    Java Syntaxis task 0435 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0435/Solution.java
@@ -6,7 +6,10 @@ package com.javarush.task.task04.task0435;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+
+        for (int i = 2; i <= 100; i += 2){
+            System.out.println(i);
+        }
 
     }
 }
