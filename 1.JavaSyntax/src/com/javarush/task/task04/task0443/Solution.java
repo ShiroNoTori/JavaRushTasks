1e7ca29e7a83dfa225d253f47ef92486dbccd220
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 20:33:17 +0300

    Java Syntaxis task 0443 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0443/Solution.java
@@ -6,9 +6,18 @@ package com.javarush.task.task04.task0443;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int year = sc.nextInt();
+        int month = sc.nextInt();
+        int date = sc.nextInt();
+
+        System.out.println("Меня зовут " + name + "." + "\n" +
+                "Я родился " + date + "." + month + "." + year);
     }
 }
