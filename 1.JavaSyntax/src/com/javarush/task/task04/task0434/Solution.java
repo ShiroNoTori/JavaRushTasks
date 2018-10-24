72b0be7b9a9d9b50e3f702a62b82a73ceed8c300
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 19:19:28 +0300

    Java Syntaxis task 0434 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0434/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0434/Solution.java
@@ -9,6 +9,16 @@ import java.io.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        
+        int i = 1;
+        int j = 1;
+        while (i <= 10){
+            while (j <= 10){
+                System.out.print(i * j + " ");
+                j++;
+            }
+            i++;
+            j = 1;
+            System.out.println();
+        }
     }
 }
