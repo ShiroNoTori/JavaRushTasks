44c0b82852ff2b6d8492eaf4eed1f41de5881309
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 19:02:01 +0300

    Java Syntaxis task 0429 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0429/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0429/Solution.java
@@ -5,10 +5,30 @@ package com.javarush.task.task04.task0429;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        int[] m = new int[3];
+
+        for (int i = 0; i < m.length; i++){
+            m[i] = sc.nextInt();
+        }
+
+        int positiveN = 0;
+        int negativeN = 0;
+
+        for (int i = 0; i < m.length; i++){
+            if (m[i] == 0) continue;
+
+            if (m[i] > 0) positiveN++;
+            else negativeN++;
+        }
+
+        System.out.println("количество отрицательных чисел: " + negativeN + '\n'
+                + "количество положительных чисел: " + positiveN);
 
     }
 }
