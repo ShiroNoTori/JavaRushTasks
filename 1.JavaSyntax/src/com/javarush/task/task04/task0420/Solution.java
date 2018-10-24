15316f6fd496b3bd28d82955b0783b65c023cb9c
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 17:54:10 +0300

    Java Syntaxis task 0420 completed. Как-то очень надуманно получилось.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -5,9 +5,43 @@ package com.javarush.task.task04.task0420;
 */
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        int[] m = new int[3];
+        m[0] += sc.nextInt();
+        m[1] += sc.nextInt();
+        m[2] += sc.nextInt();
+
+
+        for (int i = 1; i <= m.length; i++){
+
+            int max = m[m.length - 1];
+            int counter = m.length - 1;
+            int buffer;
+
+
+            for (int j = i - 1; j < m.length - 1; j++){
+                if (m[j] > max){
+                    max = m[j];
+                    counter = j;
+                }
+            }
+
+            System.out.print(max + " ");
+            buffer = m[i - 1];
+            m[i - 1] = max;
+            m[counter] = buffer;
+
+        }
+
+
+
+
     }
 }
