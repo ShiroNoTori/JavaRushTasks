bac648f07184213de67c1aff176b3e1953a9b1ce
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 18:02:58 +0300

    Java Syntaxis task 0423 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0423/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0423/Solution.java
@@ -5,9 +5,16 @@ package com.javarush.task.task04.task0423;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        int age = sc.nextInt();
+
+        if (age > 20)
+            System.out.println("И 18-ти достаточно");
     }
 }
