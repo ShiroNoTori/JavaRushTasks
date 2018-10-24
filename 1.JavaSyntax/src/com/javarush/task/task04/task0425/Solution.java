c74fde4dc3457ce4405d7d3bf8be651c15b19bb8
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 18:16:59 +0300

    Java Syntaxis task 0425 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0425/Solution.java
@@ -5,9 +5,24 @@ package com.javarush.task.task04.task0425;
 */
 
 import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (x != 0 && y != 0){
+            if (x > 0 && y > 0)
+                System.out.println(1);
+            else if (x < 0 && y > 0)
+                System.out.println(2);
+            else if (x < 0 && y < 0)
+                System.out.println(3);
+            else if (x > 0 && y < 0)
+                System.out.println(4);
+        }
     }
 }
