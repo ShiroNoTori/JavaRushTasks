1324695564cecde1255aa405d06856a450094143
ShiroNoTori <accelerator1569@gmail.com>
2018-10-24 19:40:23 +0300

    Java Syntaxis task 0437 completed.

--- 1.JavaSyntax/src/com/javarush/task/task04/task0437/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0437/Solution.java
@@ -7,7 +7,13 @@ package com.javarush.task.task04.task0437;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+
+        for (int i = 1; i <= 10; i++){
+            for (int j = 1; j <= i; j++){
+                System.out.print(8);
+            }
+            System.out.println();
+        }
 
     }
 }
