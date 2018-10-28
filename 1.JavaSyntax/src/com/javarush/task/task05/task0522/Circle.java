6cb9d22fa35bfa62e775cff1ee773847853cadcb
ShiroNoTori <accelerator1569@gmail.com>
2018-10-29 00:48:26 +0300

    Java Syntaxis task 0522 completed.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0522/Circle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0522/Circle.java
@@ -9,7 +9,27 @@ public class Circle {
     public double y;
     public double radius;
 
-    //напишите тут ваш код
+    public Circle(){
+        this(1, 2, 3);
+    }
+
+    public Circle(double x, double y, double radius){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public Circle(double x, double radius){
+        this.x = x;
+        this.y = x;
+        this.radius = radius;
+    }
+
+    public Circle(double x){
+        this.x = x;
+        this.y = x;
+        this.radius = 4;
+    }
 
     public static void main(String[] args) {
 
