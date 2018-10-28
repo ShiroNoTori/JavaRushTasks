a4a298e03eef0c7630b735dd457c8a08ccf6b014
ShiroNoTori <accelerator1569@gmail.com>
2018-10-28 23:12:29 +0300

    Java Syntaxis task 0513 completed.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0513/Rectangle.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0513/Rectangle.java
@@ -5,7 +5,38 @@ package com.javarush.task.task05.task0513;
 */
 
 public class Rectangle {
-    //напишите тут ваш код
+    public int top;
+    public int left;
+    public int width;
+    public int height;
+
+    public void initialize(int left, int top, int width, int height){
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void initialize(int left, int top){
+        this.left = left;
+        this.top = top;
+        this.width = 0;
+        this.height = 0;
+    }
+
+    public void initialize(int left, int top, int width){
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = width;
+    }
+
+    public void initialize(Rectangle anotherRectangle){
+        this.left = anotherRectangle.left;
+        this.top = anotherRectangle.top;
+        this.width = anotherRectangle.width;
+        this.height = anotherRectangle.height;
+    }
 
     public static void main(String[] args) {
 
