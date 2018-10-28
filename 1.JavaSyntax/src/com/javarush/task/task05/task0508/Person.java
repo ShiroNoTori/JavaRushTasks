e2ca12f7eeadcafe0d30a0e9edb037c643962165
ShiroNoTori <accelerator1569@gmail.com>
2018-10-28 22:13:15 +0300

    Java Syntaxis task 0508 completed.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0508/Person.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0508/Person.java
@@ -5,7 +5,33 @@ package com.javarush.task.task05.task0508;
 */
 
 public class Person {
-    //напишите тут ваш код
+    String name;
+    int age;
+    char sex;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
 
     public static void main(String[] args) {
 
