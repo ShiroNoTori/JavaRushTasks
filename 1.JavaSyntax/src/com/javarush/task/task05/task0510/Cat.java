1d3c9136345a04851e2ba647476dd4246312d3ac
ShiroNoTori <accelerator1569@gmail.com>
2018-10-28 22:34:35 +0300

    Java Syntaxis task 0510 completed.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0510/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0510/Cat.java
@@ -5,9 +5,54 @@ package com.javarush.task.task05.task0510;
 */
 
 public class Cat {
-    //напишите тут ваш код
+    public String name;
+    public int age;
+    public int weight;
+    public String color;
+    public String address;
+
+    public void initialize(String name){
+        this.name = name;
+        this.age = 3;
+        this.weight = 2;
+        this.color = "black";
+        this.address = null;
+    }
+
+    public void initialize(String name, int weight, int age){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = "black";
+        this.address = null;
+    }
+
+    public void initialize(String name, int age){
+        this.name = name;
+        this.age = age;
+        this.weight = 2;
+        this.color = "black";
+        this.address = null;
+    }
+
+    public void initialize(int weight, String color){
+        this.name = null;
+        this.age = 3;
+        this.weight = weight;
+        this.color = color;
+        this.address = null;
+    }
+
+    public void initialize(int weight, String color, String address){
+        this.name = null;
+        this.age = 3;
+        this.weight = weight;
+        this.color = color;
+        this.address = address;
+    }
 
     public static void main(String[] args) {
+
 
     }
 }
