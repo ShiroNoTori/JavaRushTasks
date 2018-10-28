2b3cf8492f6921bd60aa3b984c6715f144eb1547
ShiroNoTori <accelerator1569@gmail.com>
2018-10-28 21:31:02 +0300

    Java Syntaxis task 0501 completed.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0501/Cat.java
@@ -5,7 +5,11 @@ package com.javarush.task.task05.task0501;
 */
 
 public class Cat {
-    //напишите тут ваш код
+
+    String name;
+    int age;
+    int weight;
+    int strength;
 
     public static void main(String[] args) {
 
