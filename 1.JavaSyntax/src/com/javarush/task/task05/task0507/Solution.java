802f5e84c29e354a680c986753be2179b1d3010d
ShiroNoTori <accelerator1569@gmail.com>
2018-10-28 22:11:22 +0300

    Java Syntaxis task 0507 completed. Хотел сначала сделать через условие в цикле, но валидацию это не прошло. Как мне кажется, через return не оень красиво.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -4,9 +4,30 @@ package com.javarush.task.task05.task0507;
 Среднее арифметическое
 */
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        
+        int counter = 0;
+        float summ = 0;
+        int n;
+
+        while (true){
+
+            n = sc.nextInt();
+            if (n == -1){
+                System.out.println(summ/counter);
+                return;
+            } else {
+                summ += n;
+                counter++;
+            }
+
+
+
+        }
     }
 }
 
