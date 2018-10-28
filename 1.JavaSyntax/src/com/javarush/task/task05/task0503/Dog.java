f48a33a4e8e1ff3ec5aa45cb12ab3b0887be7365
ShiroNoTori <accelerator1569@gmail.com>
2018-10-28 21:46:47 +0300

    Java Syntaxis task 0503 completed.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0503/Dog.java
@@ -6,7 +6,25 @@ package com.javarush.task.task05.task0503;
 */
 
 public class Dog {
-    //напишите тут ваш код
+
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public static void main(String[] args) {
 
