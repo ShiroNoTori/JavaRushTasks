4dcdb2a2b4777a20652a582fc6ceafd7998f6626
ShiroNoTori <accelerator1569@gmail.com>
2018-10-28 21:39:26 +0300

    Java Syntaxis task 0502 completed.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0502/Cat.java
@@ -13,10 +13,24 @@ public class Cat {
     }
 
     public boolean fight(Cat anotherCat) {
-        //напишите тут ваш код
+
+        if (this.age + this.strength + this.weight > anotherCat.age + anotherCat.strength + anotherCat.weight)
+            return true;
+        else return false;
+
     }
 
     public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.weight = 1;
+        cat1.strength = 1;
+        cat1.age = 1;
 
+        Cat cat2= new Cat();
+        cat2.weight = 2;
+        cat2.strength = 1;
+        cat2.age = 1;
+
+        System.out.println(cat2.fight(cat1));
     }
 }
