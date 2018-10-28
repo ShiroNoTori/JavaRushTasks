b58ce4dfb51ff246748bbb3e75533f431e2e5046
ShiroNoTori <accelerator1569@gmail.com>
2018-10-28 22:11:42 +0300

    Java Syntaxis task 0508

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        
+
         int counter = 0;
         float summ = 0;
         int n;
