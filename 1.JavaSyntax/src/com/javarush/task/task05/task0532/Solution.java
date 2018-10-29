469e2bd20aba6407984bb6a86091d4d0578302d3
ShiroNoTori <accelerator1569@gmail.com>
2018-10-29 21:01:41 +0300

    Java Syntaxis task 0532 Пишет, что не выводит максимум.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task05.task0532;
 
 import java.io.*;
+import java.lang.Math;
 
 /* 
 Задача по алгоритмам
@@ -9,10 +10,17 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = ;
 
-        //напишите тут ваш код
+        int n = Integer.parseInt(reader.readLine());
+        int a;
+        int maximum = n;
 
+        for (int i = 0; i < n; i++){
+            a = Integer.parseInt(reader.readLine());
+            if (a > maximum)
+                maximum = a;
+        }
+        
         System.out.println(maximum);
     }
 }
