a59dc46608722b07448fc0a2c4535bde1e4097cd
ShiroNoTori <accelerator1569@gmail.com>
2018-10-29 23:09:01 +0300

    Java Syntaxis task 0601 completed.

--- 1.JavaSyntax/src/com/javarush/task/task06/task0601/Cat.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0601/Cat.java
@@ -5,7 +5,11 @@ package com.javarush.task.task06.task0601;
 */
 
 public class Cat {
-    //напишите тут ваш код
+
+    protected void finalize() throws Throwable{
+
+    }
+
     public static void main(String[] args) {
 
     }
