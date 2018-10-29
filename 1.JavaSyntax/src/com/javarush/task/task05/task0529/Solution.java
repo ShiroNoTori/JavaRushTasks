e27c4150f94652523de3e1cca7a18cd68293c919
ShiroNoTori <accelerator1569@gmail.com>
2018-10-29 20:27:37 +0300

    Java Syntaxis task 0529 completed. Не сзару вспомнил, что ссылочные типы данных нельзя сравнивать стандартным оператором.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task05.task0529;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* 
 Консоль-копилка
@@ -9,6 +10,23 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+
+        int summ = 0;
+        String n;
+        boolean isOff = false;
+
+        while (!isOff){
+            n = sc.nextLine();
+            if (n.equals("сумма")){
+                isOff = true;
+                System.out.println(summ);
+            } else {
+                summ += Integer.parseInt(n);
+            }
+
+
+        }
+
     }
 }
