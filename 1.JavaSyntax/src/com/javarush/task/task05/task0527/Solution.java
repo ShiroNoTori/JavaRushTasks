a9ff5575e9afffb09d36c03094854345a6c1e0a9
ShiroNoTori <accelerator1569@gmail.com>
2018-10-29 20:13:14 +0300

    Java Syntaxis task 0527 completed.

--- 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0527/Solution.java
@@ -7,8 +7,8 @@ package com.javarush.task.task05.task0527;
 public class Solution {
     public static void main(String[] args) {
         Mouse jerryMouse = new Mouse("Jerry", 12, 5);
-
-        //напишите тут ваш код
+        Cat tomCat = new Cat("Tom", 50, 30);
+        Dog billDog = new Dog("Bill", 70, 40);
     }
 
     public static class Mouse {
@@ -23,5 +23,27 @@ public class Solution {
         }
     }
 
-    //напишите тут ваш код
+    public static class Cat {
+        String name;
+        int height;
+        int tail;
+
+        public Cat(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
+
+    public static class Dog {
+        String name;
+        int height;
+        int tail;
+
+        public Dog(String name, int height, int tail) {
+            this.name = name;
+            this.height = height;
+            this.tail = tail;
+        }
+    }
 }
