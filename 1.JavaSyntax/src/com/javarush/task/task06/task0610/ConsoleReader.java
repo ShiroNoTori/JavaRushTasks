896784e3fc9cea5786691794658a27ea45c06b82
ShiroNoTori <accelerator1569@gmail.com>
2018-10-30 17:51:17 +0300

    Java Syntaxis task 0610 completed. Почему в условии написано, что нужно создавать в каждом методе scanner, если 1 статический сканер на весь класс работает нормально.

--- 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -2,33 +2,33 @@ package com.javarush.task.task06.task0610;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /* 
 Класс ConsoleReader
 */
 
 public class ConsoleReader {
-    public static String readString() throws Exception {
-        //напишите тут ваш код
 
+
+    //static Scanner sc = new Scanner(System.in);
+
+    public static String readString() throws Exception {
+        return new Scanner(System.in).nextLine();
     }
 
     public static int readInt() throws Exception {
-        //напишите тут ваш код
-
+        return new Scanner(System.in).nextInt();
     }
 
     public static double readDouble() throws Exception {
-        //напишите тут ваш код
-
+        return new Scanner(System.in).nextDouble();
     }
 
     public static boolean readBoolean() throws Exception {
-        //напишите тут ваш код
-
+        return new Scanner(System.in).nextBoolean();
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
     }
 }
