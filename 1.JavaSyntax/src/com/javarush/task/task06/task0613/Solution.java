c6a28492633a49e8d9185c33d061b0179bf8aded
ShiroNoTori <accelerator1569@gmail.com>
2018-10-30 20:17:10 +0300

    Java Syntaxis task 0613 completed.

--- 1.JavaSyntax/src/com/javarush/task/task06/task0613/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0613/Solution.java
@@ -6,14 +6,19 @@ package com.javarush.task.task06.task0613;
 
 public class Solution {
     public static void main(String[] args) {
-        // Создай 10 котов
+        for (int i = 0; i < 10; i++){
+            Cat cat = new Cat();
+        }
 
-        // Выведи значение переменной catCount
+        System.out.println(Cat.catCount);
     }
 
     public static class Cat {
-        // Создай статическую переменную catCount
+        public static int catCount;
 
-        // Создай конструктор
+        public Cat(){
+            catCount++;
+        }
+
     }
 }
