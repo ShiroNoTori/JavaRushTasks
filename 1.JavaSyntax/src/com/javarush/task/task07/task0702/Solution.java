ef519489e3ea7586be6cefc7042bcaee4da1b62c
ShiroNoTori <accelerator1569@gmail.com>
2018-10-31 02:16:30 +0300

    Java Syntaxis task 0702 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0702/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0702/Solution.java
@@ -9,6 +9,18 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arr = new String[10];
+
+        for (int i = 0; i < 8; i++){
+            arr[i] = reader.readLine();
+        }
+
+        for (int i = 1; i <= arr.length; i++){
+            System.out.println(arr[arr.length - i]);
+        }
+
     }
 }
