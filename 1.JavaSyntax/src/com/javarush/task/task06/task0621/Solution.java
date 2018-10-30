97523f2a76d3f8ed17f971919481d3f669ee97ea
ShiroNoTori <accelerator1569@gmail.com>
2018-10-30 23:57:32 +0300

    Java Syntaxis task 0621 completed. Каквя-то муторная ерунда. Причём, все требования узнаёшь только после валидации.

--- 1.JavaSyntax/src/com/javarush/task/task06/task0621/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0621/Solution.java
@@ -12,35 +12,53 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        String grandpaName = reader.readLine();
+        String grandmaName = reader.readLine();
+        String fatherMane = reader.readLine();
         String motherName = reader.readLine();
-        Cat catMother = new Cat(motherName);
-
+        String sonName = reader.readLine();
         String daughterName = reader.readLine();
-        Cat catDaughter = new Cat(daughterName, catMother);
 
+        Cat catGrandpa = new Cat(grandpaName);
+        Cat catGrandma = new Cat(grandmaName);
+        Cat catFather = new Cat(fatherMane, catGrandpa, null);
+        Cat catMother = new Cat(motherName, null, catGrandma);
+        Cat catSon = new Cat(sonName, catFather, catMother);
+        Cat catDaughter = new Cat(daughterName, catFather, catMother);
+
+        System.out.println(catGrandpa);
+        System.out.println(catGrandma);
+        System.out.println(catFather);
         System.out.println(catMother);
+        System.out.println(catSon);
         System.out.println(catDaughter);
     }
 
     public static class Cat {
         private String name;
-        private Cat parent;
+        private Cat father;
+        private Cat mother;
 
         Cat(String name) {
             this.name = name;
         }
 
-        Cat(String name, Cat parent) {
+
+        Cat(String name, Cat father, Cat mother) {
             this.name = name;
-            this.parent = parent;
+            this.father = father;
+            this.mother = mother;
         }
 
         @Override
         public String toString() {
-            if (parent == null)
-                return "The cat's name is " + name + ", no mother ";
-            else
-                return "The cat's name is " + name + ", mother is " + parent.name;
+            if (father == null && mother == null)
+                return "The cat's name is " + name + ", no mother, no father";
+            else if (father != null && mother == null)
+                return "The cat's name is " + name + ", no mother, father is " + father.name;
+            else if (father == null && mother != null)
+                return "The cat's name is " + name + ", mother is " + mother.name + ", no father";
+            else return "The cat's name is " + name + ", mother is " + mother.name + ", father is " +  father.name;
         }
     }
 
