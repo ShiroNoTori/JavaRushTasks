462ce5cd680d8424d42e7722aad900705729e240
ShiroNoTori <accelerator1569@gmail.com>
2018-10-31 14:30:48 +0300

    Java Syntaxis task 0712 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -11,6 +11,34 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            strings.add(reader.readLine());
+        }
+
+        int min = strings.get(0).length();
+        int max = 0;
+        int indexMin = 0;
+        int indexMax = 0;
+
+        for (int i = 0; i < strings.size(); i++){
+            int currentSize = strings.get(i).length();
+            if (currentSize > max){
+                max = currentSize;
+                indexMax = i;
+            }
+            if (currentSize < min){
+                min = currentSize;
+                indexMin = i;
+            }
+        }
+
+        if (indexMax < indexMin)
+            System.out.println(strings.get(indexMax));
+        else System.out.println(strings.get(indexMin));
+
     }
 }
