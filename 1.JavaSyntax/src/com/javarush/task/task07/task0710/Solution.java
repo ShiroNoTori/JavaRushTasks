48e8f61571bde368e3ac99b3099cf44b0dc946c4
ShiroNoTori <accelerator1569@gmail.com>
2018-10-31 14:01:30 +0300

    Java Syntaxis task 0710 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0710/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0710/Solution.java
@@ -10,6 +10,15 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            strings.add(0, reader.readLine());
+        }
+
+        for (int i = 0; i < strings.size(); i++){
+            System.out.println(strings.get(i));
+        }
     }
 }
