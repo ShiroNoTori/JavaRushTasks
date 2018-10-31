f733b4992f5155d3664f0cc367a68f47432d0925
ShiroNoTori <accelerator1569@gmail.com>
2018-10-31 13:58:19 +0300

    Java Syntaxis task 0709 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0709/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0709/Solution.java
@@ -9,7 +9,30 @@ import java.util.ArrayList;
 */
 
 public class Solution {
+
+
+
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> strings = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++){
+            strings.add(reader.readLine());
+        }
+
+        int minLength = strings.get(0).length();
+
+        for (int i = 0; i < strings.size(); i++){
+            int currentLength = strings.get(i).length();
+            if (currentLength < minLength){
+                minLength = currentLength;
+            }
+        }
+
+        for (int i = 0; i < strings.size(); i++){
+            if (strings.get(i).length() == minLength)
+                System.out.println(strings.get(i));
+        }
     }
 }
