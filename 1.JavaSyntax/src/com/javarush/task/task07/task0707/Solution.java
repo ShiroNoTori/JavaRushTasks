a1e967b6d085ca0e0ab7d3dbef388df43028c94e
ShiroNoTori <accelerator1569@gmail.com>
2018-10-31 13:43:36 +0300

    Java Syntaxis task 0707 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0707/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0707/Solution.java
@@ -8,6 +8,13 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        ArrayList<String> strList = new ArrayList<>();
+        for (int i = 0; i < 5; i++){
+            strList.add(Integer.toString(i));
+        }
+        System.out.println(strList.size());
+        for (int i = 0; i < strList.size(); i++){
+            System.out.println(strList.get(i));
+        }
     }
 }
