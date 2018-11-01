e472e40b21c0351518aea24470f02eab2e6ce06f
ShiroNoTori <accelerator1569@gmail.com>
2018-11-01 20:45:23 +0300

    Java Syntaxis task 0713 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0713/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0713/Solution.java
@@ -11,10 +11,39 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> divByThree = new ArrayList<Integer>();
+        ArrayList<Integer> divByTwo = new ArrayList<Integer>();
+        ArrayList<Integer> remaining = new ArrayList<Integer>();
+
+        for (int i = 0; i < 20; i++){
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+
+
+        for (int i = 0; i < list.size(); i++){
+            int currentValue = list.get(i);
+
+            if (currentValue%3 == 0 && currentValue%2 == 0){
+                divByThree.add(currentValue);
+                divByTwo.add(currentValue);
+            } else if (currentValue%3 == 0)
+                divByThree.add(currentValue);
+            else if (currentValue%2 == 0)
+                divByTwo.add(currentValue);
+            else remaining.add(currentValue);
+        }
+
+        printList(divByThree);
+        printList(divByTwo);
+        printList(remaining);
     }
 
     public static void printList(List<Integer> list) {
-        //напишите тут ваш код
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
     }
 }
