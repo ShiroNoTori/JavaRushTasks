ec6fd21cb5bb1984eb7e70f79598dab125e5f6f2
ShiroNoTori <accelerator1569@gmail.com>
2018-11-01 20:54:26 +0300

    Java Syntaxis task 0715 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0715/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0715/Solution.java
@@ -10,6 +10,18 @@ import java.util.ArrayList;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("мама");
+        list.add("мыла");
+        list.add("раму");
+
+        list.add(1, "именно");
+        list.add(3, "именно");
+        list.add("именно");
+
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
     }
 }
