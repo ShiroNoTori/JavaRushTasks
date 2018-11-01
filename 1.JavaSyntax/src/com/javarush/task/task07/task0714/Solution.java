07ada58be60c6a5eeb92304ea866a50a688f558b
ShiroNoTori <accelerator1569@gmail.com>
2018-11-01 20:49:35 +0300

    Java Syntaxis task 0714 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
@@ -11,7 +11,19 @@ import java.util.Collections;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        for (int i = 0; i < 5; i++){
+            list.add(reader.readLine());
+        }
+
+        list.remove(2);
+
+        for (int i = list.size() - 1; i >= 0; i--){
+            System.out.println(list.get(i));
+        }
     }
 }
 
