7d3ddc7f9a6b3e1b9dff31c64683a4140f766a98
ShiroNoTori <accelerator1569@gmail.com>
2018-11-03 02:20:59 +0300

    Java Syntaxis task 0728 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0728/Solution.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task07.task0728;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /* 
 В убывающем порядке
@@ -9,8 +12,10 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int[] array = new int[20];
+
         for (int i = 0; i < 20; i++) {
             array[i] = Integer.parseInt(reader.readLine());
         }
@@ -23,6 +28,28 @@ public class Solution {
     }
 
     public static void sort(int[] array) {
-        //напишите тут ваш код
+
+
+
+        for (int i = 0; i < array.length; i++){
+
+            int max = array[i];
+            int temp;
+            int count = i;
+
+            for (int j = i; j < array.length; j++){
+                if (array[j] > max){
+                    max = array[j];
+                    count = j;
+                }
+
+            }
+
+            temp = array[i];
+            array[i] = array[count];
+            array[count] = temp;
+
+        }
+
     }
 }
