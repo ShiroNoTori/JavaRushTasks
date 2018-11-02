b07bac36507120d90e0d7dd53183d979ced299f6
ShiroNoTori <accelerator1569@gmail.com>
2018-11-02 19:21:22 +0300

    Java Syntaxis task 0716 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0716/Solution.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws Exception {
         ArrayList<String> list = new ArrayList<String>();
+
         list.add("роза"); // 0
         list.add("лоза"); // 1
         list.add("лира"); // 2
@@ -20,7 +21,32 @@ public class Solution {
     }
 
     public static ArrayList<String> fix(ArrayList<String> list) {
-        //напишите тут ваш код
-        return null;
+
+        for (int i = 0; i < list.size(); i++){
+
+            String currentStr = list.get(i);
+
+            boolean hasP = false;
+            boolean hasL = false;
+
+            for (int j = 0; j < currentStr.length(); j++){
+                if (currentStr.charAt(j) == 'л') {
+                    hasL = true;
+                }
+                if (currentStr.charAt(j) == 'р') {
+                    hasP = true;
+                }
+            }
+
+            if (hasL && !hasP){
+                list.add(i+1, currentStr);
+                i++;
+            }
+            else if (hasP && !hasL){
+                list.remove(i);
+                i--;
+            }
+        }
+        return list;
     }
 }
