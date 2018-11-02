433c50a3b2007ca33302bae1923c15cf0062c013
ShiroNoTori <accelerator1569@gmail.com>
2018-11-02 22:25:04 +0300

    Java Syntaxis task 0720 completed.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0720/Solution.java
@@ -3,6 +3,8 @@ package com.javarush.task.task07.task0720;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /* 
 Перестановочка подоспела
@@ -12,6 +14,25 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        //напишите тут ваш код
+        ArrayList<String> list = new ArrayList<>();
+
+        int N = Integer.parseInt(reader.readLine());
+        int M = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < N; i++){
+            list.add(reader.readLine());
+        }
+
+        for (int i = 0; i < M; i++){
+
+            String temp = list.get(0);
+            list.remove(0);
+            list.add(list.size(), temp);
+        }
+
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
     }
 }
