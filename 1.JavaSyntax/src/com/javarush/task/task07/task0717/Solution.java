37a7182e9466e42c4f21e5ccbf56a7e323dab049
ShiroNoTori <accelerator1569@gmail.com>
2018-11-02 19:41:56 +0300

    Java Syntaxis task 0717 completed. Ну это на уровне магии и случайных цифирок.

--- 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task07.task0717;
 
+import javax.sound.midi.Soundbank;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -11,14 +12,27 @@ import java.util.ArrayList;
 public class Solution {
     public static void main(String[] args) throws Exception {
         // Считать строки с консоли и объявить ArrayList list тут
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<String>();
+
+        for (int i = 0; i < 10; i++){
+            list.add(reader.readLine());
+        }
 
         ArrayList<String> result = doubleValues(list);
 
-        // Вывести на экран result
+        for (int i = 0; i < result.size(); i++){
+            System.out.println(result.get(i));
+        }
     }
 
     public static ArrayList<String> doubleValues(ArrayList<String> list) {
-        //напишите тут ваш код
-        return null;
+
+        int size = 2*list.size()-1;
+
+        for (int i = 0; i <= size; i+=2){
+            list.add(i, list.get(i));
+        }
+        return list;
     }
 }
