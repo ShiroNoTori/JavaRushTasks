63f96c9df1ac5cad7e6bd97bf5757acda77fd2b1
ShiroNoTori <accelerator1569@gmail.com>
2018-11-05 23:13:47 +0300

    Java Syntaxis task 0815 completed.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0815/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0815/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task08.task0815;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 /* 
 Перепись населения
@@ -9,18 +10,35 @@ import java.util.HashSet;
 
 public class Solution {
     public static HashMap<String, String> createMap() {
-        //напишите тут ваш код
+        HashMap<String, String> map = new HashMap<String, String>();
 
+        for (int i = 0; i < 10; i++){
+            map.put(Integer.toString(i), Integer.toString(0));
+        }
+
+        return map;
     }
 
     public static int getCountTheSameFirstName(HashMap<String, String> map, String name) {
-        //напишите тут ваш код
+        int count = 0;
 
+        for (Map.Entry<String, String> pair : map.entrySet()){
+            if (pair.getValue().equals(name))
+                count++;
+        }
+
+        return count;
     }
 
     public static int getCountTheSameLastName(HashMap<String, String> map, String lastName) {
-        //напишите тут ваш код
+        int count = 0;
 
+        for (Map.Entry<String, String> pair : map.entrySet()){
+            if (pair.getKey().equals(lastName))
+                count++;
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
