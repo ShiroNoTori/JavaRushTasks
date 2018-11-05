9f6ce90734c32c25727c85b74bfb4078dff2e3eb
ShiroNoTori <accelerator1569@gmail.com>
2018-11-05 20:48:51 +0300

    Java Syntaxis task 0814 completed. Решение дурацкое.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
@@ -1,24 +1,39 @@
 package com.javarush.task.task08.task0814;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /* 
 Больше 10? Вы нам не подходите
 */
 
 public class Solution {
-    public static HashSet<Integer> createSet() {
-        // напишите тут ваш код
 
+    public static HashSet<Integer> createSet() {
+        HashSet<Integer> set = new HashSet<Integer>();
+
+        for (int i = 0; i < 20; i++){
+            set.add(i);
+        }
+
+        return set;
     }
 
     public static HashSet<Integer> removeAllNumbersGreaterThan10(HashSet<Integer> set) {
-        // напишите тут ваш код
 
+        ArrayList<Integer> list = new ArrayList<>(set);
+
+        Iterator<Integer> iterator = list.iterator();
+
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i) > 10) {
+                list.remove(i);
+                i--;
+            }
+        }
+
+        return new HashSet<Integer>(list);
     }
 
     public static void main(String[] args) {
-
     }
 }
