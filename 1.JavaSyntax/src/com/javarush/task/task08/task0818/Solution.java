d8a9d23158e158cc6d4378ce99cd9d7d2f1f0c30
ShiroNoTori <accelerator1569@gmail.com>
2018-11-06 02:29:48 +0300

    Java Syntaxis task 0818 completed.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0818/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0818/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task08.task0818;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /* 
@@ -9,14 +10,33 @@ import java.util.Map;
 
 public class Solution {
     public static HashMap<String, Integer> createMap() {
-        //напишите тут ваш код
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("asd", 1000);
+        map.put("qwe", 500);
+        map.put("wer", 100);
+        map.put("sdf", 10);
+        map.put("zxc", 100);
+
+        map.put("xcv", 500);
+        map.put("fgh", 100);
+        map.put("rty", 1000);
+        map.put("vbn", 100);
+        map.put("uyi", 1000);
+
+        return map;
     }
 
     public static void removeItemFromMap(HashMap<String, Integer> map) {
-        //напишите тут ваш код
+
+        Iterator<Map.Entry<String, Integer>> iterator = map.entrySet().iterator();
+
+        while (iterator.hasNext()){
+            if (iterator.next().getValue() < 500)
+                iterator.remove();
+        }
     }
 
     public static void main(String[] args) {
-
     }
 }
