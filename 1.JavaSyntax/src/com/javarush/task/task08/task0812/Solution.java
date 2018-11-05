e6983c5b7c8d25783384f21e1972a771131cf9ff
ShiroNoTori <accelerator1569@gmail.com>
2018-11-05 19:45:38 +0300

    Java Syntaxis task 0812 completed.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -2,13 +2,35 @@ package com.javarush.task.task08.task0812;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /* 
 Cамая длинная последовательность
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < 10; i++){
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+
+        int maxSuccession = 0;
+        int currentSuccession = 1;
+
+        for (int i = 1; i < list.size(); i++){
+
+            if (list.get(i).equals(list.get(i - 1))){
+                currentSuccession++;
+            }   else currentSuccession = 1;
+
+            if (currentSuccession > maxSuccession)
+                maxSuccession = currentSuccession;
+        }
+
+        System.out.println(maxSuccession);
 
     }
 }
