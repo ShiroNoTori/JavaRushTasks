4d30de85072afc52856ea6037e75647b81cf01a0
ShiroNoTori <accelerator1569@gmail.com>
2018-11-05 23:45:18 +0300

    Java Syntaxis task 0816 completed. Какая-то ерунда.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0816/Solution.java
@@ -3,29 +3,49 @@ package com.javarush.task.task08.task0816;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
+import java.util.*;
 
 /* 
 Добрая Зинаида и летние каникулы
 */
 
 public class Solution {
-    public static HashMap<String, Date> createMap() throws ParseException {
-        DateFormat df = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
-        HashMap<String, Date> map = new HashMap<String, Date>();
-        map.put("Stallone", df.parse("JUNE 1 1980"));
 
-        //напишите тут ваш код
+    public static HashMap<String, Date> createMap() throws ParseException {
+
+        DateFormat df = new SimpleDateFormat("MMMMM d yyyy", Locale.ENGLISH);
+
+        HashMap<String, Date> map = new HashMap<String, Date>();
+
+        map.put("Stallone", df.parse("JUNE 1 1980"));
+        map.put("Stallon", df.parse("JUNE 1 1980"));
+        map.put("Stallo", df.parse("JUNE 1 1980"));
+        map.put("Stall", df.parse("JUNE 1 1980"));
+        map.put("Stal", df.parse("JUNE 1 1980"));
+
+        map.put("Sta", df.parse("JUNE 1 1980"));
+        map.put("St", df.parse("JUNE 1 1980"));
+        map.put("S", df.parse("JUNE 1 1980"));
+        map.put("S1", df.parse("JUNE 1 1980"));
+        map.put("St1", df.parse("JUNE 1 1980"));
+
+
+        return map;
     }
 
     public static void removeAllSummerPeople(HashMap<String, Date> map) {
-        //напишите тут ваш код
 
+        Iterator<Map.Entry<String, Date>> iterator = map.entrySet().iterator();
+
+        while (iterator.hasNext()){
+            HashMap.Entry<String, Date> s = iterator.next();
+            if (s.getValue().getMonth() >= 5 && s.getValue().getMonth() <= 7){
+                iterator.remove();
+            }
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
     }
 }
