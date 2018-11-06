4878ca2a61b94306469bce3458b1ef737a43add2
ShiroNoTori <accelerator1569@gmail.com>
2018-11-06 22:32:32 +0300

    Java Syntaxis task 0828 completed.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0828/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0828/Solution.java
@@ -3,10 +3,7 @@ package com.javarush.task.task08.task0828;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /* 
 Номер месяца
@@ -14,6 +11,29 @@ import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //напишите тут ваш код
+        HashMap<Integer, String> months = new HashMap<>();
+
+        months.put(1, "January");
+        months.put(2, "February");
+        months.put(3, "March");
+        months.put(4, "April");
+        months.put(5, "May");
+        months.put(6, "June");
+        months.put(7, "July");
+        months.put(8, "August");
+        months.put(9, "September");
+        months.put(10, "October");
+        months.put(11, "November");
+        months.put(12, "December");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = reader.readLine();
+
+        for (Map.Entry<Integer, String> pair : months.entrySet()){
+            if (pair.getValue().equals(str))
+                System.out.println(str + " is the " + pair.getKey() + " month");
+        }
+
     }
 }
