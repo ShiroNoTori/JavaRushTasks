18b92abebaa86e4cd3d172c4ce894c38d5050a22
ShiroNoTori <accelerator1569@gmail.com>
2018-11-06 05:54:28 +0300

    Java Syntaxis task 0823 completed.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task08.task0823;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 /* 
 Омовение Рамы
@@ -13,6 +14,17 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
 
-        //напишите тут ваш код
+        char[] arr = s.toCharArray();
+
+        arr[0] = Character.toUpperCase(arr[0]);
+
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == ' ')
+                arr[i + 1] = Character.toUpperCase(arr[i + 1]);
+        }
+
+        s = String.valueOf(arr);
+
+        System.out.println(s);
     }
 }
