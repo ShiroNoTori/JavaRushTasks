f9123b1f86af07623e62772365abcb142b04f626
ShiroNoTori <accelerator1569@gmail.com>
2018-11-06 04:06:00 +0300

    Java Syntaxis task 0819 completed.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0819/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0819/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task08.task0819;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /* 
@@ -11,19 +12,35 @@ public class Solution {
     public static void main(String[] args) {
         Set<Cat> cats = createCats();
 
-        //напишите тут ваш код. step 3 - пункт 3
+        Iterator<Cat> iterator = cats.iterator();
+        iterator.next();
+        iterator.remove();
 
         printCats(cats);
     }
 
     public static Set<Cat> createCats() {
-        //напишите тут ваш код. step 2 - пункт 2
-        return null;
+
+        HashSet<Cat> cats = new HashSet<Cat>();
+
+        cats.add(new Cat());
+        cats.add(new Cat());
+        cats.add(new Cat());
+
+        return cats;
     }
 
     public static void printCats(Set<Cat> cats) {
-        // step 4 - пункт 4
+        Iterator<Cat> iterator = cats.iterator();
+
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
     }
 
-    // step 1 - пункт 1
+    public static class Cat{
+        public Cat(){
+
+        }
+    }
 }
