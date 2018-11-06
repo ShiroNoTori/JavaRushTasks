c5af6c76e80f5d089ab1850741048aa6f8978f6e
ShiroNoTori <accelerator1569@gmail.com>
2018-11-06 17:04:57 +0300

    Java Syntaxis task 0826 completed.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0826/Solution.java
@@ -2,6 +2,8 @@ package com.javarush.task.task08.task0826;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /* 
 Пять победителей
@@ -9,10 +11,12 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int[] array = new int[20];
         for (int i = 0; i < array.length; i++) {
             array[i] = Integer.parseInt(reader.readLine());
+            
         }
 
         sort(array);
@@ -25,6 +29,22 @@ public class Solution {
     }
 
     public static void sort(int[] array) {
-        //напишите тут ваш код
+        for (int i = 0; i < array.length; i++){
+            int max = array[i];
+            int counter = i;
+            int temp;
+
+            for (int j = i; j < array.length; j++){
+                if(array[j] >= max){
+                    max = array[j];
+                    counter = j;
+                }
+            }
+
+            temp = array[i];
+            array[i] = max;
+            array[counter] = temp;
+        }
+
     }
 }
