3fcef977fafc8418ee0302be8694cd2edaf125cb
ShiroNoTori <accelerator1569@gmail.com>
2018-11-06 19:59:07 +0300

    Java Syntaxis task 0827 Не проходит, но работает.

--- 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task08.task0827;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /* 
@@ -8,10 +9,19 @@ import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(isDateOdd("MAY 1 2013"));
+        System.out.println(isDateOdd("FEBRUARY 1 2019"));
     }
 
     public static boolean isDateOdd(String date) {
-        return true;
+        Date date1 = new Date(date);
+        Date date2 = new Date();
+        
+        date2.setTime(-10800000);           //разница в 3 часа по МСК
+
+        date2.setYear(date1.getYear());
+
+        long nOfDays = (date1.getTime() - date2.getTime())/86_400_000;
+
+        return nOfDays%2 != 0;
     }
 }
