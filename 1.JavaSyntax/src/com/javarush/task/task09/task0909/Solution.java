df141757a990fb3820cc662faf44346038e95f3e
ShiroNoTori <accelerator1569@gmail.com>
2018-11-10 00:15:30 +0300

    Java Syntaxis task 0909 completed.

--- 1.JavaSyntax/src/com/javarush/task/task09/task0909/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0909/Solution.java
@@ -6,11 +6,11 @@ package com.javarush.task.task09.task0909;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-
-        int[] m = new int[2];
-        m[8] = 5;
-
-        //напишите тут ваш код
+        try{
+            int[] m = new int[2];
+            m[8] = 5;
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }
     }
 }
