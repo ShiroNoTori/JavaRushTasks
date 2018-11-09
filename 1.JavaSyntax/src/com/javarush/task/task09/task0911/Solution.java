35cb54ea68be7d097d011afc8610a44ecde420f6
ShiroNoTori <accelerator1569@gmail.com>
2018-11-10 00:20:05 +0300

    Java Syntaxis task 0911 completed.

--- 1.JavaSyntax/src/com/javarush/task/task09/task0911/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0911/Solution.java
@@ -8,12 +8,12 @@ import java.util.HashMap;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
-
-        HashMap<String, String> map = new HashMap<String, String>(null);
-        map.put(null, null);
-        map.remove(null);
-
-        //напишите тут ваш код
+        try {
+            HashMap<String, String> map = new HashMap<String, String>(null);
+            map.put(null, null);
+            map.remove(null);
+        } catch (NullPointerException e){
+            System.out.println(e);
+        }
     }
 }
