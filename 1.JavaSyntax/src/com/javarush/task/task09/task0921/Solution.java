f5de49c890852ca093d6b4c0326a14f9be870c5f
ShiroNoTori <accelerator1569@gmail.com>
2018-11-16 02:03:13 +0300

    Java Syntaxis task 0921 completed. Правильно ли так поступать с IOE?

--- 1.JavaSyntax/src/com/javarush/task/task09/task0921/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0921/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task09.task0921;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +16,23 @@ public class Solution {
     }
 
     public static void readData() {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        boolean isInterrupted = false;
+
+        try {
+            while (!isInterrupted) {
+                list.add(Integer.parseInt(reader.readLine()));
+            }
+        } catch (NumberFormatException e) {
+            isInterrupted = true;
+            for (Integer i : list) {
+                System.out.println(i);
+            }
+        } catch (IOException e) {}
+
+
     }
 }
