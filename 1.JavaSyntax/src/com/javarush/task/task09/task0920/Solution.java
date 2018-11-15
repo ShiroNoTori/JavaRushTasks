4673136715e6899bdcae4f97df1ceb1d8b60bc86
ShiroNoTori <accelerator1569@gmail.com>
2018-11-16 01:52:59 +0300

    Java Syntaxis task 0920 completed.

--- 1.JavaSyntax/src/com/javarush/task/task09/task0920/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0920/Solution.java
@@ -8,8 +8,11 @@ public class Solution {
     public static void main(String[] args) {
         for (int i = 10; i >= 0; i--) {
             System.out.println(i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+            }
 
-            //напишите тут ваш код
         }
     }
 }
