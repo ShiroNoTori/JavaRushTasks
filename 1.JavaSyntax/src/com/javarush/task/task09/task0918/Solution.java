8114a232ebfb4efa2ab333a6df1a869848071288
ShiroNoTori <accelerator1569@gmail.com>
2018-11-16 01:46:44 +0300

    Java Syntaxis task 0919

--- 1.JavaSyntax/src/com/javarush/task/task09/task0918/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0918/Solution.java
@@ -7,17 +7,17 @@ package com.javarush.task.task09.task0918;
 public class Solution {
     public static void main(String[] args) {
     }
-    
+
     static class MyException extends Error {
     }
 
-    static class MyException2 extends RuntimeException{
+    static class MyException2 extends RuntimeException {
     }
 
-    static class MyException3 extends Throwable{
+    static class MyException3 extends Throwable {
     }
 
-    static class MyException4 extends Exception{
+    static class MyException4 extends Exception {
     }
 }
 
